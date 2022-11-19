c55007cadf6ed877b0d664da9e83a7ecdb4cb540
marcos.queiroz.ext <marcos.queiroz.ext@infracommerce.com.br>
2022-11-19 10:09:06 -0300

    feat: :sparkles: finalização do modulo 8

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class SiteContato extends Model
 {
     //
+
     protected $table = 'site_contatos';
+    protected $fillable = ['nome', 'email', 'telefone', 'motivo_contato', 'mensagem'];
 }
